5bf049fe9c1ff6f979675daa7fe73a509dd6bca8
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2025-12-27 19:19:59 -0500

    multus

--- dot_config/fish/conf.d/sway.fish
+++ dot_config/fish/conf.d/sway.fish
@@ -1,15 +1,5 @@
 # If running from tty1 start sway
 set TTY1 (tty)
 if test "$TTY1" = "/dev/tty1"
-    # TODO: runit user services
-    /usr/bin/gnome-keyring-daemon --start
-    dbus-update-activation-environment --all
-    dbus-update-activation-environment XDG_CURRENT_DESKTOP=sway WAYLAND_DISPLAY
-    gpg-connect-agent /bye
-    /usr/libexec/polkit-gnome-authentication-agent-1 &
-    pipewire &
-    kdeconnectd &
-    syncthing serve --no-browser --no-restart --logflags=0 &
-
     exec dbus-run-session sway
 end
