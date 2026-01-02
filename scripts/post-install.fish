189e82bbb627983abbb824146fdc6388d2c290e4
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-01-02 15:27:09 -0500

    feat: npd

--- scripts/post-install.fish
+++ scripts/post-install.fish
@@ -2,6 +2,8 @@ cd $HOME/.local/share/chezmoi/
 gh extension install gennaro-tedesco/gh-f
 git remote add origin git@github.com:prncss-xyz/chezmoi.git || true
 mkdir -p ~/.config/service/dbus
+mkdir -p ~/.local/state/mpd
+
 ln -s /usr/share/examples/turnstile/dbus.run ~/.config/service/dbus/run
 ln -s /usr/share/examples/turnstile/dbus.check ~/.config/service/dbus/check
 
