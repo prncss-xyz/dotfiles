677d2379e4d04eaedf66dac851b360df79ed04f9
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-01-29 21:25:24 -0500

    multus

--- scripts/push-files.fish
+++ scripts/push-files.fish
@@ -1,4 +1,6 @@
 cd
 rsync -avz --exclude '*.lock' .gnupg $USER@$ARGv[1]:.gnupg
-rsync -avz --files-from=push-files.txt . $USER@$ARGV[1]:
+rsync -avz -e 'ssh -p 2222' --exclude '*.lock' .gnupg prncss@192.168.167.160:.gnupg
+rsync -avz --files-from=push-files.txt . $USER@$ARGV[pass1]:
+# rsync -av --ignore-existing .ssh/* prncss@odonata.local:.ssh/
 # rsync -av --ignore-existing .ssh/* prncss@odonata.local:.ssh/
