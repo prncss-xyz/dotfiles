049d95a5c2c542d1c714c9410fad04b34a2f1bb0
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-02-16 19:14:33 -0500

    multus

--- dot_config/fish/functions/gc.fish
+++ dot_config/fish/functions/gc.fish
@@ -1,29 +1,26 @@
 function gc
-  read -P "Message: " -l msg
-  if test -n "$msg"
-    set -l prefixes \
-    "feat" \
-    "fix" \
-    "docs" \
-    "style" \
-    "refactor" \
-    "perf" \
-    "test" \
-    "chore" \
-    "ci" \
-    "build"
+  set -l prefixes \
+  "feat" \
+  "fix" \
+  "docs" \
+  "style" \
+  "refactor" \
+  "perf" \
+  "test" \
+  "chore" \
+  "ci" \
+  "build"
 
-    set -l selected (printf "%s\n" $prefixes | fzf --height=40% --prompt="Select commit type: ")
+  set -l selected (printf "%s\n" $prefixes | fzf --height=40% --prompt="Select commit type: ")
 
-    if test -z "$selected"
-      echo "Commit cancelled"
-      return 1
-    end
-
-    set -l prefix (string split ":" $selected)[1]
-
-    git commit -m "$prefix: $msg" $argv
-  else
-      git commit --amend --no-edit
+  if test -z "$selected"
+    echo "Commit cancelled"
+    return 1
   end
+
+  read -P "Message: " -l msg
+
+  set -l prefix (string split ":" $selected)[1]
+
+  git commit -m "$prefix: $msg" $argv
 end
