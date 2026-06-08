81c910ac0738b1b8500b67c6e539641662a4a22a
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-06-08 18:33:14 -0400

    changes from crotte on Mon Jun 8 18:33:14 EDT 2026

--- dot_config/fish/functions/gwta.fish
+++ dot_config/fish/functions/gwta.fish
@@ -1,8 +1,8 @@
-function gwta --description "Create a git worktree and install dependencies" --argument-names NAME
+function gwta --description "Create a git worktree" --argument-names NAME
     if not test -n "$NAME"
         echo "Usage: gwta <branch-name>"
         return 1
     end
 
-    git worktree add $NAME $argv[2..] && cd $NAME && i
+    git worktree add $NAME $argv[2..]
 end
